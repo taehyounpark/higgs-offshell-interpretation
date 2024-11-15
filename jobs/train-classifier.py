3da2bf4014cd4a6957e3f99bde19ad2872572a4a
maxgriese <max.griese@tum.de>
2024-11-15 20:50:59 +0100

    Standard scaler implemented in train-classifier

--- jobs/train-classifier.py
+++ jobs/train-classifier.py
@@ -3,8 +3,7 @@ sys.path.append('../')
 
 import tensorflow as tf
 from tensorflow import keras
-from tensorflow.keras.optimizers import Nadam # type: ignore
-from sklearn.preprocessing import StandardScaler
+from tensorflow.keras.optimizers import Nadam 
 
 from hstar import process
 from hstar import trilinear
@@ -19,14 +18,19 @@ import numpy as np
 
 import os
 
-print('Number of GPUs available: ', len(tf.config.list_physical_devices('GPU')))
+from sklearn.preprocessing import StandardScaler
+
+
+strategy = tf.distribute.MirroredStrategy()
+print('Number of devices: ', strategy.num_replicas_in_sync)
 
 SEED=373485
 GEN=2
 
-OUTPUT_DIR='../outputs/def'
-SAMPLE_DIR='../..'
+# GEN=2, SEED=373485: c6=20, maxi net (10 layers, 2k nodes each), (added early stopping) epochs=100, train:val = 50:50
 
+OUTPUT_DIR='../outputs/single'
+SAMPLE_DIR='../..'
 
 sample = process.Sample(weight='wt', 
     amplitude = process.Basis.SBI, components = {
@@ -37,41 +41,57 @@ sample = process.Sample(weight='wt',
   })
 
 sample.open(csv = [
-  SAMPLE_DIR + '/ggZZ2e2m_all.csv',
-  SAMPLE_DIR + '/ggZZ4e_all.csv',
-  SAMPLE_DIR + '/ggZZ4m_all.csv'
-  ], xs=[1.4783394, 0.47412769, 0.47412769], lumi=3000., k=1.83, nrows=300000
+  SAMPLE_DIR + '/ggZZ2e2m_all_new.csv',
+  SAMPLE_DIR + '/ggZZ4e_all_new.csv',
+  SAMPLE_DIR + '/ggZZ4m_all_new.csv'
+  ], xs=[1.4783394, 0.47412769, 0.47412769], lumi=3000., k=1.83
 )
 
-sample.events = sample.events.sample(frac=1).reset_index(drop=True)
+print('Total events:', sample.events.shape[0])
+
+base_size = 500000 # for train and validation data each
+
+fraction = 2*base_size/sample.events.shape[0] # fraction of the dataset that is actually needed
+
+sample.events = sample.events.sample(frac=fraction, random_state=SEED, ignore_index=True) # shuffle dataset and take out the specified fraction
 
 zmasses = zcandidate.ZmassPairChooser(sample)
 leptons = zmasses.find_Z()
 
-kin_variables = angles.calculate(leptons.T[0], leptons.T[1], leptons.T[2], leptons.T[3])
+kin_variables  = angles.calculate(leptons.T[0], leptons.T[1], leptons.T[2], leptons.T[3])
 
-c6_values = np.linspace(-10,10,21)
+true_size = kin_variables.shape[0]
+
+print(f'Initial base size set to {base_size}. Train and validation data will be {int(true_size/2)*2} each after Z mass cuts.')
+
+c6 = np.linspace(-20,20,41)
 
 c6_mod = trilinear.Modifier(c6_values = [-5,-1,0,1,5], c6_amplitudes = ['msq_sbi_c6_6', 'msq_sbi_c6_10', 'msq_sbi_c6_11', 'msq_sbi_c6_12', 'msq_sbi_c6_16'])
-c6_weights = c6_mod.modify(sample=sample, c6=c6_values)
+c6_weights = c6_mod.modify(sample=sample, c6=c6)
 
-train_data = datasets.build_dataset_tf(x_arr = kin_variables, 
-                                       param_values = c6_values, 
-                                       signal_weights = c6_weights, 
-                                       background_weights = np.array(sample.events['wt']),
+train_data = datasets.build_dataset_tf(x_arr = kin_variables[:int(true_size/2)], 
+                                       param_values = c6, 
+                                       signal_weights = c6_weights[:int(true_size/2)], 
+                                       background_weights = np.array(sample.events['wt'])[:int(true_size/2)],
                                        normalization = 1)
 
-train_data = tf.random.shuffle(train_data, SEED)
+val_data = datasets.build_dataset_tf(  x_arr = kin_variables[int(true_size/2):], 
+                                       param_values = c6, 
+                                       signal_weights = c6_weights[int(true_size/2):], 
+                                       background_weights = np.array(sample.events['wt'])[int(true_size/2):],
+                                       normalization = 1)
 
-print(train_data, train_data.shape)
 
-scaler = StandardScaler()
-scaled_data = scaler.fit_transform(train_data[:,:9])
 
-train_data = tf.concat([scaled_data, train_data[:,9:]], axis=1)
+train_scaler = StandardScaler()
+train_data = tf.concat([train_scaler.fit_transform(train_data[:,:9]), train_data[:,9:]], axis=1)
 
-print(train_data)
+print('train_data:', train_data, train_data.shape)
 
+val_scaler = StandardScaler()
+val_data = tf.concat([val_scaler.fit_transform(val_data[:,:9]), val_data[:,9:]], axis=1)
+
+print('val_data:', val_data, val_data.shape)
 
 model = models.C6_4l_clf_maxi()
 
@@ -90,12 +110,14 @@ os.makedirs(OUTPUT_DIR + '/history/', exist_ok=True)
 
 checkpoint_filepath = OUTPUT_DIR + f'/ckpt/checkpoint.model_{GEN}.keras'
 model_checkpoint_callback = keras.callbacks.ModelCheckpoint(filepath=checkpoint_filepath, monitor='val_loss', mode='min', save_best_only=True)
+early_stopping_callback = keras.callbacks.EarlyStopping(monitor='val_loss', mode='min', patience=5, start_from_epoch=20)
 
-history_callback = model.fit(x=train_data[:,:9], y=train_data[:,9][:,np.newaxis], sample_weight=train_data[:,10][:,np.newaxis], validation_split=0.3, batch_size=64, callbacks=[model_checkpoint_callback], epochs=100, verbose=2)
-
+history_callback = model.fit(x=train_data[:,:9], y=train_data[:,9][:,np.newaxis], sample_weight=train_data[:,10][:,np.newaxis], validation_data=(val_data[:,:9], val_data[:,9], val_data[:,10]), batch_size=64, callbacks=[model_checkpoint_callback, early_stopping_callback], epochs=100, verbose=2)
 
 model.save(OUTPUT_DIR + f'/models/model_{GEN}.keras')
 
 with open(OUTPUT_DIR + f'/history/history_{GEN}.txt', 'w') as hist_file:
     hist_file.write(str(history_callback.history['loss']))
     hist_file.write(str(history_callback.history['val_loss']))
+
+print(model.summary())
