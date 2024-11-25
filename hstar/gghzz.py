28be45e636a4fbc8324824d46bb81f01ad1d9364
maxgriese <max.griese@tum.de>
2024-11-25 14:41:05 +0100

    Fixed bug with nrows argument

--- hstar/gghzz.py
+++ hstar/gghzz.py
@@ -60,7 +60,7 @@ class Process():
     for sample_from_channel in channels:
       xsec = sample_from_channel[0]
       filepath = sample_from_channel[1]
-      nrows = None if len(sample_from_channel) < 2 else sample_from_channel[2]
+      nrows = None if len(sample_from_channel) < 3 else sample_from_channel[2]
       df = pd.read_csv(filepath, nrows=nrows)
       kinematics_per_channel.append(df[mcfm.kinematics])
       amplitudes_per_channel.append(df[mcfm.amplitudes])
