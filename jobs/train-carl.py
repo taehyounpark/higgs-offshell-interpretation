d69d8fe526bd6327f80c8bb05429f9d1e30935b1
maxgriese <max.griese@tum.de>
2024-12-10 16:46:23 +0100

    Refactored model building and loading for carl.

--- jobs/train-carl.py
+++ jobs/train-carl.py
@@ -1,8 +1,6 @@
 import numpy as np
 import os, json
 import sys
-sys.path.append('../')
-
 import warnings
 warnings.filterwarnings("ignore", category=DeprecationWarning) 
 
@@ -15,7 +13,8 @@ from physics.simulation import msq
 
 from physics.hzz import zpair, angles
 
-from nn import datasets, models
+from nn import models
+from nn.carl import dataset, model
 
 from sklearn.preprocessing import StandardScaler
 
@@ -36,7 +35,7 @@ def load_config(config_path):
 
     # Only one of the component flags can be activated at once
     component_flags = np.array(['sig-vs-sig', 'sig-vs-sbi', 'int-vs-sbi', 'bkg-vs-sbi'])
-    num_c_flags = np.sum(np.array([c_flag in config[0]['flags'] for c_flag in component_flags]).astype(int))
+    num_c_flags = np.sum(np.array([c_flag in config['flags'] for c_flag in component_flags]).astype(int))
 
     if num_c_flags > 1:
         raise ValueError('You can only activate one of [sig-vs-sig, sig-vs-sbi, int-vs-sbi, bkg-vs-sbi] at once')
@@ -45,14 +44,14 @@ def load_config(config_path):
     flags_active = []
     flags_possible = ['distributed', 'sig-vs-sig', 'sig-vs-sbi', 'int-vs-sbi', 'bkg-vs-sbi']
     for flag in flags_possible:
-        if flag in config[0]['flags']:
+        if flag in config['flags']:
             flags_active.append(flag)
 
     # For BKG no c6 is needed
     if 'bkg-vs-sbi' in flags_active:
         c6_input = np.array([0.0])
     else:
-        c6_input = np.fromstring(config[0]['c6_values'].replace('[','').replace(']',''), sep=',')
+        c6_input = np.fromstring(config['c6_values'].replace('[','').replace(']',''), sep=',')
 
     # Build c6 array from the input to the c6 argument
     if len(c6_input) == 1:
@@ -63,116 +62,18 @@ def load_config(config_path):
         raise ValueError('c6 should be a single value or three comma separated values like a,b,c specifying a np.linspace(a,b,c)')
     
     # Build num_nodes array from the input to the num-nodes argument
-    n_nodes_input = np.fromstring(config[0]['num_nodes'].replace('[','').replace(']',''), sep=',')
+    n_nodes_input = np.fromstring(str(config['num_nodes']).replace('[','').replace(']',''), sep=',').astype(int)
     if len(n_nodes_input) == 1:
         num_nodes = n_nodes_input.item()
-    elif len(n_nodes_input) == config[0]['num_layers']:
+    elif len(n_nodes_input) == config['num_layers']:
         num_nodes = n_nodes_input.tolist()
     else:
         raise ValueError('num-nodes should be a single value or a comma separated list of integer values with a length equals len(num-nodes)=num-layers')
     
+    # Load sample dir from config
+    sample_dir = '/'.join([os.environ[el[1:]] if '$' in el else el for el in config['sample_dir'].split('/')])
 
-    return {'sample_dir': args.sample_dir, 'output_dir': args.output_dir, 'flags': flags_active, 'learning_rate': args.learn_rate, 'batch_size': args.batch_size, 'num_events': args.num_events, 'num_layers': args.num_layers, 'num_nodes': num_nodes, 'epochs': args.epochs, 'c6_values': c6_values.tolist()}
-
-def load_samples(config, component_1, component_2):
-    if config['num_events'] is None:
-        n_i = None
-    else:
-        n_i = int(config['num_events']*1.2)#int(2*config['num_events']/3)
-
-    def match_comp(config, component, n_i):
-        match component:
-            case msq.Component.SIG:
-                sample = gghzz.Process(msq.Component.SIG, (0.1, os.path.join(config['sample_dir'], 'ggZZ2e2m_sig.csv'), n_i))
-            case msq.Component.SBI:
-                sample = gghzz.Process(msq.Component.SBI, (1.5, os.path.join(config['sample_dir'], 'ggZZ2e2m_sbi.csv'), n_i))
-            case msq.Component.BKG:
-                sample = gghzz.Process(msq.Component.BKG, (1.6, os.path.join(config['sample_dir'], 'ggZZ2e2m_bkg.csv'), n_i))
-            case msq.Component.INT:
-                sample = gghzz.Process(msq.Component.INT, (-0.2, os.path.join(config['sample_dir'], 'ggZZ2e2m_int.csv'), n_i))
-        return sample
-
-    return (match_comp(config, component_1, n_i), match_comp(config, component_2, n_i))
-
-
-
-def load_kinematics(sample, bounds1=(50,115), bounds2=(50,115), algorithm='leastsquare'):
-    z_chooser = zpair.ZPairChooser(bounds1=bounds1, bounds2=bounds2, algorithm=algorithm)
-
-    return angles.calculate(*sample.events.filter(z_chooser))
-
-
-def save_config(output_dir, *config):
-    file_path = os.path.join(output_dir, 'job.config')
-
-    os.makedirs(output_dir, exist_ok=True)
-    with open(file_path, 'w') as config_file:
-        config_file.write(json.dumps(config, indent=4))
-
-def get_components(config):
-    component_flag = np.array(config['flags'])[np.where([ (flag in ['sig', 'int', 'sig-vs-sbi', 'int-vs-sbi', 'bkg-vs-sbi']) for flag in config['flags'] ])]
-    component_flag = component_flag[0] if component_flag.shape[0] != 0 else 'sbi'
-    component_1, component_2 = component_flag.split('-')[0], component_flag.split('-')[-1]
-    
-    comp_dict = {'sig': msq.Component.SIG,
-                 'int': msq.Component.INT,
-                 'bkg': msq.Component.BKG,
-                 'sbi': msq.Component.SBI}
-
-    return (comp_dict[component_1], comp_dict[component_2])
-    
-
-def build_model(config, strategy=None):
-    if 'distributed' in config['flags'] and strategy is not None:
-        with strategy.scope():
-            if len(config['c6_values']) == 1:
-                model = models.C6_4l_clf(num_layers=config['num_layers'], num_nodes=config['num_nodes'], input_dim=8)
-            else:
-                model = models.C6_4l_clf(num_layers=config['num_layers'], num_nodes=config['num_nodes'], input_dim=9)
-
-            optimizer = Nadam(
-                learning_rate=config['learning_rate'],
-                beta_1=0.9,
-                beta_2=0.999,
-                epsilon=1e-07
-            )
-
-            model.compile(optimizer=optimizer, loss='binary_crossentropy', metrics=['binary_accuracy'], weighted_metrics=['binary_accuracy'])
-    else:
-        if len(config['c6_values']) == 1:
-            model = models.C6_4l_clf(num_layers=config['num_layers'], num_nodes=config['num_nodes'], input_dim=8)
-        else:
-            model = models.C6_4l_clf(num_layers=config['num_layers'], num_nodes=config['num_nodes'], input_dim=9)
-
-        optimizer = Nadam(
-            learning_rate=config['learning_rate'],
-            beta_1=0.9,
-            beta_2=0.999,
-            epsilon=1e-07
-        )
-
-        model.compile(optimizer=optimizer, loss='binary_crossentropy', metrics=['binary_accuracy'], weighted_metrics=['binary_accuracy'])
-    
-    return model
-
-def train_model(model, config, training_data, validation_data, callbacks=None, strategy=None):
-    train_dataset = tf.data.Dataset.from_tensor_slices((training_data[:,:-2], training_data[:,-2][:,tf.newaxis], training_data[:,-1][:,tf.newaxis]))
-    val_dataset = tf.data.Dataset.from_tensor_slices((validation_data[:,:-2], validation_data[:,-2][:,tf.newaxis], validation_data[:,-1][:,tf.newaxis]))
-
-    if 'distributed' in config['flags'] and strategy is not None:
-        with strategy.scope():
-            train_dataset = train_dataset.batch(config['batch_size']*strategy.num_replicas_in_sync)
-            val_dataset = val_dataset.batch(config['batch_size']*strategy.num_replicas_in_sync)
-
-            history_callback = model.fit(train_dataset, validation_data=val_dataset, callbacks=callbacks, epochs=config['epochs'], verbose=2)
-    else:
-        train_dataset = train_dataset.batch(config['batch_size'])
-        val_dataset = val_dataset.batch(config['batch_size'])
-
-        # Run model.fit
-        history_callback = model.fit(train_dataset, validation_data=val_dataset, callbacks=callbacks, epochs=config['epochs'], verbose=2)
-        
-    return history_callback
+    return {'sample_dir': sample_dir, 'flags': flags_active, 'learning_rate': config['learning_rate'], 'batch_size': config['batch_size'], 'num_events': config['num_events'], 'num_layers': config['num_layers'], 'num_nodes': num_nodes, 'epochs': config['epochs'], 'c6_values': c6_values.tolist()}
 
 
 def main(config):
@@ -182,93 +83,23 @@ def main(config):
     if 'distributed' in config['flags']:
         print(f'Model will be training on {mirrored_strategy.num_replicas_in_sync} GPUs')
 
-    component_1, component_2 = get_components(config)
-
-    sample_1, sample_2 = load_samples(config, component_1, component_2)
-
-    set_size_1, set_size_2 = sample_1.events.kinematics.shape[0], sample_2.events.kinematics.shape[0]
-
-    sample_1.events.filter(msq.MSQFilter('msq_bkg_sm', value=0.0))
-    sample_1.events.filter(msq.MSQFilter('msq_bkg_sm', value=np.nan))
-    sample_2.events.filter(msq.MSQFilter('msq_bkg_sm', value=0.0))
-    sample_2.events.filter(msq.MSQFilter('msq_bkg_sm', value=np.nan))
-
-    kin_vars_1, kin_vars_2 = load_kinematics(sample_1), load_kinematics(sample_2)
-
-    sample_1.events = sample_1.events[:int(config['num_events'])]
-    kin_vars_1 = kin_vars_1[:int(config['num_events'])]
-
-    sample_2.events = sample_2.events[:int(config['num_events'])]
-    kin_vars_2 = kin_vars_2[:int(config['num_events'])]
-
-    true_size_1, true_size_2 = kin_vars_1.shape[0], kin_vars_2.shape[0]
-
-    print(f'Initial base size of {["SIG", "INT", "BKG", "SBI"][component_1.value-1]} set to {int(set_size_1)}. Train and validation data will be {int(true_size_1/2)*2} each after Z mass cuts.')
-    print(f'Initial base size of {["SIG", "INT", "BKG", "SBI"][component_2.value-1]} set to {int(set_size_2)}. Train and validation data will be {int(true_size_2/2)*2} each after Z mass cuts.')
-    print(f'Total dataset size after filters (per train, val): {int(true_size_1/2) + int(true_size_2/2)}')
-
-    if component_1 != msq.Component.BKG:
-        c6_mod = c6.Modifier(baseline = component_1, c6_values = [-5,-1,0,1,5])
-        sig_weights, sig_prob = c6_mod.modify(sample=sample_1, c6=config['c6_values'])
-    else:
-        sig_weights, sig_prob = np.array(sample_1.events.weights)[:,np.newaxis], np.array(sample_1.events.probabilities)[:,np.newaxis]
-    
-    if component_1 == msq.Component.INT: # TODO: Fix this somehow
-        c6_mod = c6.Modifier(baseline = component_1, c6_values = [-5,0,5])
-        sig_weights, sig_prob = c6_mod.modify(sample=sample_1, c6=config['c6_values'])
-        sig_weights, sig_prob = -1 * sig_weights, -1 * sig_prob
-
-    train_data = datasets.build_dataset_random(x_arr_sig = kin_vars_1[:int(true_size_1/2)], 
-                                               x_arr_bkg = kin_vars_2[:int(true_size_2/2)],
-                                               param_values = config['c6_values'],
-                                               signal_probabilities = sig_prob[:int(true_size_1/2)],
-                                               background_probabilities = np.array(sample_2.events.probabilities)[:int(true_size_2/2)],
-                                               seed = SEED)
-    
-    val_data = datasets.build_dataset_random(x_arr_sig = kin_vars_1[int(true_size_1/2):],
-                                             x_arr_bkg = kin_vars_2[int(true_size_2/2):],
-                                             param_values = config['c6_values'],
-                                             signal_probabilities = sig_prob[int(true_size_1/2):],
-                                             background_probabilities = np.array(sample_2.events.probabilities)[int(true_size_2/2):],
-                                             seed = SEED)
-    
-    # The following will scale only kinematics for nonprm and kinematics + c6 for prm
-    train_scaler = StandardScaler()
-    train_data = tf.concat([train_scaler.fit_transform(train_data[:,:-2]), train_data[:,-2:]], axis=1)
-    train_data = tf.random.shuffle(train_data, seed=SEED)
-
-    val_data = tf.concat([train_scaler.transform(val_data[:,:-2]), val_data[:,-2:]], axis=1)
-    val_data = tf.random.shuffle(val_data, seed=SEED)
-
-    save_config(config['output_dir'], config, {'scaler.mean_': train_scaler.mean_.tolist(), 'scaler.var_': train_scaler.var_.tolist(), 'scaler.scale_': train_scaler.scale_.tolist()})
-    print(f'Settings for this run are stored in {os.path.join(config["output_dir"], "job.config")}')
+    # Build datasets (distributed if flag given)
+    train_dataset, val_dataset = dataset.build(config, SEED, mirrored_strategy)
 
     # Build model (distributed if flag given)
-    model = build_model(config, mirrored_strategy)
-
-    os.makedirs(config['output_dir'], exist_ok=True)
-
-    # Setup keras callbacks
-    checkpoint_filepath = os.path.join(config['output_dir'], 'checkpoint.model.tf')
-    model_checkpoint_callback = keras.callbacks.ModelCheckpoint(filepath=checkpoint_filepath, monitor='val_loss', mode='min', save_best_only=True, save_format='tf')
-    early_stopping_callback = keras.callbacks.EarlyStopping(monitor='val_loss', mode='min', patience=30, start_from_epoch=30)
-    tensorboard_callback = keras.callbacks.TensorBoard(log_dir=os.path.join(config['output_dir'], 'logs'))
+    model_carl = model.build(config, mirrored_strategy)
 
     # Train model
-    history_callback = train_model(model, config, train_data, val_data, callbacks=[model_checkpoint_callback, tensorboard_callback], strategy=mirrored_strategy)
+    history_callback = model.train(model_carl, config, train_dataset, val_dataset, strategy=mirrored_strategy)
     
-    model.save(os.path.join(config['output_dir'], 'final.model.tf'), save_format='tf')
+    # Save model
+    model.save(model_carl, history_callback)
 
-    with open(os.path.join(config['output_dir'], 'history.txt'), 'w') as hist_file:
-        hist_file.write(str(history_callback.history['loss']))
-        hist_file.write(str(history_callback.history['val_loss']))
-
-    print(model.summary())
+    print(model_carl.summary())
 
 
 if __name__ == '__main__':
     args = parse_arguments()
-
     config = load_config(args.config)
 
     main(config)
