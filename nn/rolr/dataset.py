b3e4c8eb27b677f0faad7fea9bd7cf412d1b42ca
maxgriese <max.griese@tum.de>
2024-12-11 11:02:53 +0100

    Implemented ROLR regression model + dataset generation and training

---
+++ nn/rolr/dataset.py
@@ -0,0 +1,144 @@
+from physics.simulation import msq
+from physics.hstar import gghzz, c6
+from physics.hzz import angles, zpair
+
+import os
+import json
+import numpy as np
+import tensorflow as tf
+
+from sklearn.preprocessing import MinMaxScaler
+
+
+def get_components(config):
+    component_flag = np.array(config['flags'])[np.where([ (flag in ['sig', 'int', 'sig-vs-sbi', 'int-vs-sbi', 'bkg-vs-sbi']) for flag in config['flags'] ])]
+    component_flag = component_flag[0] if component_flag.shape[0] != 0 else 'sbi'
+    component_1, component_2 = component_flag.split('-')[0], component_flag.split('-')[-1]
+    
+    comp_dict = {'sig': msq.Component.SIG,
+                 'int': msq.Component.INT,
+                 'bkg': msq.Component.BKG,
+                 'sbi': msq.Component.SBI}
+
+    return (comp_dict[component_1], comp_dict[component_2])
+
+def build_dataset(x_arr, param_values, signal_probabilities, background_probabilities):
+    data = []
+
+    non_prm=False
+
+    if np.isscalar(param_values):
+        param_values = [param_values]
+        non_prm=True
+    elif len(param_values) == 1:
+        non_prm=True
+
+    for i in range(len(param_values)):
+        param = param_values[i]
+        # x | param | y | weight
+        if not non_prm:
+            inputs = tf.concat([x_arr, tf.ones(x_arr.shape[0])[:,tf.newaxis]*param], axis=1)
+            targets = tf.cast(signal_probabilities.T[i][:,tf.newaxis]/background_probabilities[:,tf.newaxis], tf.float32)
+
+            data.append(tf.concat([inputs, targets], axis=0))
+        else:
+            inputs = x_arr
+            targets = tf.cast(signal_probabilities.T[i][:,tf.newaxis]/background_probabilities[:,tf.newaxis], tf.float32)
+
+            data.append(tf.concat([inputs, targets], axis=0))
+
+    data = tf.reshape(tf.convert_to_tensor(data), (tf.convert_to_tensor(data).shape[0]*tf.convert_to_tensor(data).shape[1], tf.convert_to_tensor(data).shape[2]))
+
+    return data
+
+def load_sample(config, component):
+    if config['num_events'] is None:
+        n_i = None
+    else:
+        n_i = int(config['num_events']*1.2)
+
+    def match_comp(config, component, n_i):
+        match component:
+            case msq.Component.SIG:
+                sample = gghzz.Process(msq.Component.SIG, (0.1, os.path.join(config['sample_dir'], 'ggZZ2e2m_sig.csv'), n_i))
+            case msq.Component.SBI:
+                sample = gghzz.Process(msq.Component.SBI, (1.5, os.path.join(config['sample_dir'], 'ggZZ2e2m_sbi.csv'), n_i))
+            case msq.Component.BKG:
+                sample = gghzz.Process(msq.Component.BKG, (1.6, os.path.join(config['sample_dir'], 'ggZZ2e2m_bkg.csv'), n_i))
+            case msq.Component.INT:
+                sample = gghzz.Process(msq.Component.INT, (-0.2, os.path.join(config['sample_dir'], 'ggZZ2e2m_int.csv'), n_i))
+        return sample
+
+    return match_comp(config, component, n_i)
+
+def load_kinematics(sample, bounds1=(70,115), bounds2=(70,115), algorithm='leastsquare'):
+    z_chooser = zpair.ZPairChooser(bounds1=bounds1, bounds2=bounds2, algorithm=algorithm)
+
+    # calculate_2 uses y4l as additional kinematic
+    return angles.calculate_2(*sample.events.filter(z_chooser))
+
+def build(config, seed, strategy=None):
+    component_1, component_2 = get_components(config)
+
+    sample_2 = load_sample(config, component_1, component_2)
+
+    set_size_2 = sample_2.events.kinematics.shape[0]
+
+    sample_2.events.filter(msq.MSQFilter('msq_bkg_sm', value=0.0))
+    sample_2.events.filter(msq.MSQFilter('msq_bkg_sm', value=np.nan))
+
+    kin_vars_2 = load_kinematics(sample_2)
+
+    sample_2.events = sample_2.events[:int(config['num_events'])]
+    kin_vars_2 = kin_vars_2[:int(config['num_events'])]
+
+    true_size_2 = kin_vars_2.shape[0]
+
+    print(f'Initial base size of {["SIG", "INT", "BKG", "SBI"][component_2.value-1]}(SM) set to {int(set_size_2)}. Train and validation data will be {int(true_size_2/2)} each after Z mass cuts.')
+    print(f'Total dataset size after filters (per train, val): {int(true_size_2/2)}')
+
+    if component_1 != msq.Component.BKG:
+        c6_mod = c6.Modifier(baseline = component_1, c6_values = [-5,-1,0,1,5])
+        sig_weights, sig_prob = c6_mod.modify(sample=sample_2, c6=config['c6_values'])
+    else:
+        sig_weights, sig_prob = np.array(sample_2.events.weights)[:,np.newaxis], np.array(sample_2.events.probabilities)[:,np.newaxis]
+    
+    if component_1 == msq.Component.INT: #TODO: Fix this somehow
+        c6_mod = c6.Modifier(baseline = component_1, c6_values = [-5,0,5])
+        sig_weights, sig_prob = c6_mod.modify(sample=sample_2, c6=config['c6_values'])
+
+    train_data = build_dataset(x_arr = kin_vars_2[:int(true_size_2/2)], 
+                               param_values = config['c6_values'],
+                               signal_probabilities = sig_prob[:int(true_size_2/2)],
+                               background_probabilities = np.array(sample_2.events.probabilities)[:int(true_size_2/2)])
+    
+    val_data = build_dataset(x_arr = kin_vars_2[int(true_size_2/2):],
+                             param_values = config['c6_values'],
+                             signal_probabilities = sig_prob[int(true_size_2/2):],
+                             background_probabilities = np.array(sample_2.events.probabilities)[int(true_size_2/2):])
+    
+    # The following will scale only kinematics for nonprm and kinematics + c6 for prm
+    train_scaler = MinMaxScaler()
+    train_data = tf.concat([train_scaler.fit_transform(train_data[:,:-1]), train_data[:,-1][:, tf.newaxis]], axis=1)
+    train_data = tf.random.shuffle(train_data, seed=seed)
+
+    val_data = tf.concat([train_scaler.transform(val_data[:,:-1]), val_data[:,-1][:, tf.newaxis]], axis=1)
+    val_data = tf.random.shuffle(val_data, seed=seed)
+
+    scaler_config = {'scaler.scale_': train_scaler.scale_, 'scaler.min_': train_scaler.min_}
+    with open('scaler.json', 'w') as scaler_file:
+        scaler_file.write(json.dumps(scaler_config, indent=4))
+
+    # Build tf Dataset objects and batch data
+    train_dataset = tf.data.Dataset.from_tensor_slices((train_data[:,:-1], train_data[:,-1][:,tf.newaxis]))
+    val_dataset = tf.data.Dataset.from_tensor_slices((val_data[:,:-1], val_data[:,-1][:,tf.newaxis]))
+
+    if 'distributed' in config['flags'] and strategy is not None:
+        with strategy.scope():
+            train_dataset = train_dataset.batch(config['batch_size']*strategy.num_replicas_in_sync)
+            val_dataset = val_dataset.batch(config['batch_size']*strategy.num_replicas_in_sync)
+    else:
+        train_dataset = train_dataset.batch(config['batch_size'])
+        val_dataset = val_dataset.batch(config['batch_size'])
+
+    return (train_dataset, val_dataset)
--- physics/hzz/angles.py
+++ physics/hzz/angles.py
@@ -77,6 +77,80 @@ def calculate(l1: MomentumObject4D, l2: MomentumObject4D, l3: MomentumObject4D, 
     
     return np.array([cth_star, cth1, cth2, phi1, phi, Z1.mass, Z2.mass, m4l]).T
 
+def calculate_2(l1: MomentumObject4D, l2: MomentumObject4D, l3: MomentumObject4D, l4: MomentumObject4D) -> np.ndarray:
+    """
+    Calculates kinematic variables cos 𝜃∗, cos 𝜃1, cos 𝜃2, 𝜙1 ,𝜙, mZ1 and mZ2. 
+    Angles used are described in https://journals.aps.org/prd/pdf/10.1103/PhysRevD.86.095031.
+
+    params:
+        l1 (MomentumObject4D): Lepton 1 used in calculations. Must be negative and paired up with l2 to form a Z boson. Can also be a vector array of MomentumObject4D objects.
+        l2 (MomentumObject4D): Lepton 2 used in calculations. Must be positive and paired up with l1 to form a Z boson. Can also be a vector array of MomentumObject4D objects.
+        l3 (MomentumObject4D): Lepton 3 used in calculations. Must be negative and paired up with l4 to form a Z boson. Can also be a vector array of MomentumObject4D objects.
+        l4 (MomentumObject4D): Lepton 4 used in calculations. Must be positive and paired up with l3 to form a Z boson. Can also be a vector array of MomentumObject4D objects.
+
+    Returns:
+        np.ndarray: The kinematic variables in form of [cos 𝜃∗, cos 𝜃1, cos 𝜃2, 𝜙1 ,𝜙, mZ1, mZ2].
+    """
+    Z1 = l1+l2
+    Z2 = l3+l4
+
+    m4l = (l1+l2+l3+l4).mass
+    y4l = (l1+l2+l3+l4).rapidity
+
+    H = Z1+Z2
+
+    # Boost both Z bosons to Higgs frame
+    Z1_h = Z1.boost(-H)
+    Z2_h = Z2.boost(-H)
+
+    # Unit vector (3D) from boosted Z1 boson
+    z1 = Z1_h.to_3D().unit()
+
+    # Angle between Z1 and the beam axis
+    cth_star = z1.z
+
+    # Boost leptons to Higgs frame
+    l1_h = l1.boost(-H)
+    l2_h = l2.boost(-H)
+    l3_h = l3.boost(-H)
+    l4_h = l4.boost(-H)
+
+    # Get 3D vectors
+    l1_h3 = l1_h.to_3D()
+    l2_h3 = l2_h.to_3D()
+    l3_h3 = l3_h.to_3D()
+    l4_h3 = l4_h.to_3D()
+
+    # Get unit vector in z direction
+    nz = vector.array({'x': np.zeros(Z1_h.shape[0]), 'y': np.zeros(Z1_h.shape[0]), 'z': np.ones(Z1_h.shape[0])})
+
+    n12 = l1_h3.cross(l2_h3).unit() # Normal vector of the plane in which the Z1 decay takes place
+    n34 = l3_h3.cross(l4_h3).unit() # Normal vector of the plane in which the Z2 decay takes place
+    nscp = nz.cross(z1).unit() # Normal vector of the plane in which the H -> Z1, Z2 takes place
+
+    # Calculate 𝜙1 ,𝜙
+    phi = z1.dot(n12.cross(n34))/np.abs(z1.dot(n12.cross(n34)))*np.arccos(-n12.dot(n34))
+
+    #legal_inds = np.where(~np.isnan(phi))[0]
+    # TODO: decide what to do if n12 and n34 are parallel
+        
+    phi1 = z1.dot(n12.cross(nscp))/np.abs(z1.dot(n12.cross(nscp)))*np.arccos(n12.dot(nscp))
+
+    # Boost Z1 to Z2 rest frame and Z2 to Z1 rest frame (does this really work like this?)
+    Z1_in_Z2 = Z1_h.boost(-Z2_h)
+    Z2_in_Z1 = Z2_h.boost(-Z1_h)
+
+    z1_in_Z2 = Z1_in_Z2.to_3D()
+    z2_in_Z1 = Z2_in_Z1.to_3D()
+
+    l1 = l1.boost(-Z1_h)
+    l3 = l3.boost(-Z2_h)
+
+    # Calculate cos 𝜃1, cos 𝜃2
+    cth1 = - z2_in_Z1.dot(l1.to_3D())/np.abs(z2_in_Z1.mag*l1.to_3D().mag)
+    cth2 = - z1_in_Z2.dot(l3.to_3D())/np.abs(z1_in_Z2.mag*l3.to_3D().mag)
+    
+    return np.array([cth_star, cth1, cth2, phi1, phi, Z1.mass, Z2.mass, m4l, y4l]).T
 
 class M4lFilter():
     def __init__(self, m4l_min=None, m4l_max=None):
