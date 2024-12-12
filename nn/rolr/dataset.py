76e3c9fd304181babb3c030780cad7350372ab80
maxgriese <max.griese@tum.de>
2024-12-12 12:42:45 +0100

    Bugfix for special case on different numpy versions (?)

--- nn/rolr/dataset.py
+++ nn/rolr/dataset.py
@@ -25,6 +25,9 @@ def get_components(config):
 def build_dataset(x_arr, param_values, signal_probabilities, background_probabilities):
     data = []
 
+    signal_probabilities = tf.convert_to_tensor(signal_probabilities)
+    background_probabilities = tf.convert_to_tensor(background_probabilities)
+
     non_prm=False
 
     if np.isscalar(param_values):
@@ -38,13 +41,13 @@ def build_dataset(x_arr, param_values, signal_probabilities, background_probabil
         # x | param | y | weight
         if not non_prm:
             inputs = tf.concat([x_arr, tf.ones(x_arr.shape[0])[:,tf.newaxis]*param], axis=1)
-            targets = tf.cast(signal_probabilities.T[i][:,tf.newaxis]/background_probabilities[:,tf.newaxis], tf.float32)
+            targets = tf.cast(signal_probabilities[:,i][:,tf.newaxis]/background_probabilities[:,tf.newaxis], tf.float32)
             weights = tf.cast(background_probabilities[:,tf.newaxis], tf.float32)
 
             data.append(tf.concat([inputs, targets, weights], axis=1))
         else:
             inputs = x_arr
-            targets = tf.cast(signal_probabilities.T[i][:,tf.newaxis]/background_probabilities[:,tf.newaxis], tf.float32)
+            targets = tf.cast(signal_probabilities[:,i][:,tf.newaxis]/background_probabilities[:,tf.newaxis], tf.float32)
             weights = tf.cast(background_probabilities[:,tf.newaxis], tf.float32)
 
             data.append(tf.concat([inputs, targets, weights], axis=1))
@@ -100,14 +103,14 @@ def build(config, seed, strategy=None):
     print(f'Total dataset size after filters (per train, val): {int(true_size_2/2)}')
 
     if component_1 != msq.Component.BKG:
-        c6_mod = c6.Modifier(baseline = component_1, c6_values = [-5,-1,0,1,5])
-        sig_weights, sig_prob = c6_mod.modify(sample=sample_2, c6=config['c6_values'])
+        c6_mod = c6.Modifier(baseline = component_1, sample=sample_2, c6_values = [-5,-1,0,1,5])
+        sig_weights, sig_prob = c6_mod.modify(c6=config['c6_values'])
     else:
         sig_weights, sig_prob = np.array(sample_2.events.weights)[:,np.newaxis], np.array(sample_2.events.probabilities)[:,np.newaxis]
     
     if component_1 == msq.Component.INT: #TODO: Fix this somehow
-        c6_mod = c6.Modifier(baseline = component_1, c6_values = [-5,0,5])
-        sig_weights, sig_prob = c6_mod.modify(sample=sample_2, c6=config['c6_values'])
+        c6_mod = c6.Modifier(baseline = component_1, sample=sample_2, c6_values = [-5,0,5])
+        sig_weights, sig_prob = c6_mod.modify(c6=config['c6_values'])
 
     train_data = build_dataset(x_arr = kin_vars_2[:int(true_size_2/2)], 
                                param_values = config['c6_values'],
